f8425f1fa9bb49aefac188be2ec4579138fcba72
le cong nghia <nghialc@bsscommerce.com>
2025-07-25 11:28:30 +0700

    refactor folder core

--- src/components/s3/dto/request/upload-file.request.dto.ts
+++ src/components/s3/dto/request/upload-file.request.dto.ts
@@ -1,25 +1,27 @@
 import { BaseDto } from '@core/dto/base.request.dto';
 import { ApiProperty } from '@nestjs/swagger';
-import { IsOptional, IsString, MaxLength } from 'class-validator';
+import { IsOptional, IsString, MaxLength, Matches } from 'class-validator';
 
 export class UploadFileRequestDto extends BaseDto<UploadFileRequestDto> {
   @ApiProperty({
-    description: 'Thư mục để lưu file (tùy chọn)',
+    description: 'Thư mục để lưu file (tùy chọn, không chứa khoảng trắng)',
     example: 'avatars',
     required: false,
   })
   @IsOptional()
   @IsString()
   @MaxLength(100)
+  @Matches(/^[^\s]+$/)
   folder?: string;
 
   @ApiProperty({
-    description: 'Tên file tùy chỉnh (tùy chọn)',
+    description: 'Tên file tùy chỉnh (tùy chọn, không chứa khoảng trắng)',
     example: 'my-custom-filename',
     required: false,
   })
   @IsOptional()
   @IsString()
   @MaxLength(100)
+  @Matches(/^[^\s]+$/)
   customFileName?: string;
 }
